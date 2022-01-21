45bd980205ff654ee45e3a3c01647652ea967655
DmitryGK <gulyakevichdmitry@gmail.com>
2022-01-21 22:31:31 +0300

    refactoring

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -31,11 +31,5 @@ type StarPropsType = {
 }
 
 function Star(props:StarPropsType) {
-    console.log('Star rendering')
-    if (props.selected) {
-        return <span><b>star </b></span>
-    } else {
-        return <span>star </span>
-    }
-
+    return  (props.selected? <span><b>star </b></span> : <span>star </span>)
 }
