f285876786b505fb11eb1019e07e5c54771421e8
DmitryGK <gulyakevichdmitry@gmail.com>
2022-02-14 03:17:20 +0300

    add items to accordion

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {action} from '@storybook/addon-actions'
 import {Accordion} from "./Accordion";
 
@@ -7,14 +7,39 @@ export default {
     component: Accordion
 }
 
-const callback = () =>  action('open and close body')
-
-export const CloseAccordionMode = () => <Accordion titleValue= 'Menu' collapsed={true} onChange={callback} />
-export const OpenAccordionMode = () => <Accordion titleValue='Users' collapsed={false} onChange={callback}/>
+const callback = () => action('open and close body')
+const onClickCallback = () => action('some item was clicked')
+export const CloseAccordionMode = () => <Accordion titleValue='Menu'
+                                                   collapsed={true}
+                                                   onChange={callback} items={[]}
+                                                   onClick={onClickCallback}/>
+export const OpenAccordionMode = () => <Accordion titleValue='Users'
+                                                  collapsed={false}
+                                                  onChange={callback}
+                                                  onClick={onClickCallback}
+                                                  items={
+                                                      [
+                                                          {title: 'Dmitry', value: 1},
+                                                          {title: 'Evgeniy', value: 2},
+                                                          {title: 'Oleg', value: 3},
+                                                          {title: 'Sveta', value: 4}
+                                                      ]
+                                                  }/>
 export const OpenCloseModeChanging = () => {
     let [value, setValue] = useState<boolean>(true)
-    return <Accordion titleValue={'title'} collapsed={value} onChange={() => {
-        setValue(!value)
-    }}
+    return <Accordion titleValue={'title'}
+                      collapsed={value}
+                      onClick={onClickCallback}
+                      items={
+                          [
+                              {title: 'Dmitry', value: 1},
+                              {title: 'Evgeniy', value: 2},
+                              {title: 'Oleg', value: 3},
+                              {title: 'Sveta', value: 4}
+                          ]
+                      }
+                      onChange={() => {
+                          setValue(!value)
+                      }}
     />
 }
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+type ItemType = {
+    title: string
+    value: any
+}
 
 type AccordionPropsType = {
     titleValue: string
     collapsed: boolean
     onChange: () => void
+    items: ItemType[]
+    onClick: (value: any) => void
 }
 
 
@@ -13,7 +19,7 @@ export function Accordion(props: AccordionPropsType) {
         return (
             <div>
                 <AccordionTitle title={props.titleValue}  onChange={props.onChange}/>
-                {!props.collapsed && <AccordionBody/>}
+                {!props.collapsed && <AccordionBody items={props.items} onClick={props.onClick}/>}
             </div>
         )
 
@@ -24,16 +30,21 @@ type AccordionTitlePropsType = {
 
 }
 
+type AccordionBodyPropsType = {
+    items: ItemType[]
+    onClick: (value: any) => void
+}
+
 function AccordionTitle(props: AccordionTitlePropsType) {
     console.log('AccordionTitle rendering')
     return <h3 onClick={() => {props.onChange()}}>{props.title}</h3>
 }
 
-function AccordionBody() {
+
+
+function AccordionBody(props: AccordionBodyPropsType) {
     return<ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+        {props.items.map((i, index) => <li key={index} onClick={() => {props.onClick(i.value)}}>{i.title}</li>)}
         </ul>
 
 }
