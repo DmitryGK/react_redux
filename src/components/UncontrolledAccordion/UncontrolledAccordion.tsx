3cc489f5edf16b5ac3b73b8c9fa979e350576b95
DmitryGK <gulyakevichdmitry@gmail.com>
2022-02-24 20:31:56 +0300

    useReducer

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,19 +1,22 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
+import {reducer, TOGGLE_COLLAPSED} from "./reducer";
 
 
 type AccordionPropsType = {
     titleValue: string
 }
 
+
 export function UncontrolledAccordion(props: AccordionPropsType) {
 
 
-    let [collapsed, setCollapsed] = useState(true)
+
+    let [state, dispatch] = useReducer(reducer, { collapsed: false})
 
         return (
             <div>
-                <AccordionTitle title={props.titleValue} onClick={() => {setCollapsed(!collapsed)}}/>
-                {!collapsed && <AccordionBody/>}
+                <AccordionTitle title={props.titleValue} onClick={() => {dispatch({type: TOGGLE_COLLAPSED})}}/>
+                {!state.collapsed && <AccordionBody/>}
             </div>
         )
 
---
+++ src/components/UncontrolledAccordion/reducer.tsx
@@ -0,0 +1,22 @@
+export type ActionType = {
+    type: string
+}
+
+export const TOGGLE_COLLAPSED = 'TOGGLE-COLLAPSED'
+
+export type StateType = {
+    collapsed: boolean
+}
+
+export const reducer = (state: StateType, action: ActionType):StateType => {
+
+    switch (action.type) {
+        case TOGGLE_COLLAPSED:
+            return {
+                ...state,
+                collapsed: !state.collapsed
+            }
+        default:
+            return state
+    }
+}
