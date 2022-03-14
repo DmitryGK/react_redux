ec52ab3fd847e54812494fb7c31a05d722ce2af1
DmitryGK <gulyakevichdmitry@gmail.com>
2022-03-15 01:05:48 +0300

    useState+

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,16 @@
+import { useState } from "@storybook/addons";
+import React from "react";
+
+
+export default {
+    title: 'useState',
+}
+
+export const Example = () => {
+    const [counter, setCounter] = useState(0)
+
+    return <>
+        <button onClick = { () => setCounter(counter + 1)}>+</button>
+        {counter}
+    </>
+}
