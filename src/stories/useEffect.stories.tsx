152373beb066ac3fa35d936fff611e66acc513d6
DmitryGK <gulyakevichdmitry@gmail.com>
2022-04-30 20:27:02 +0300

    useEffect 2nd less

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -10,23 +10,48 @@ export const SimpleExample = () => {
 
     console.log('Simple Example')
 
-    useEffect( () => {
+    useEffect(() => {
         console.log('useEffect every render');
-        
+        document.title = counter.toString()
     })
     // нет зависимостей
-    useEffect( () => {
+    useEffect(() => {
         console.log('useEffect only first render');
-        
+        document.title = counter.toString()
+
     }, [])
     // 
-    useEffect( () => {
+    useEffect(() => {
         console.log('useEffect first render and every counter changed');
-        
+        document.title = counter.toString()
+
     }, [counter])
     //зависимость от counter
     return <>
         Hello, {counter}
         <button onClick={() => setCounter(counter + 1)} >+</button>
+    </>
+}
+
+export const SetTimeoutExample = () => {
+
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetTimeout example');
+
+    useEffect (() => {
+        
+        setInterval(() => {
+           setCounter( (state) => state + 1 )
+        }, 1000)
+    }, [])
+
+    
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>+</button>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
     </>
 }
